31bae695e7ab3c1d191df107fa15906888d4348f
Praveen Sharma <praveen.sharma@decimal.co.in>
2025-03-19 23:25:32 +0530

    added cors import statement

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const { Sequelize, DataTypes } = require("sequelize");
 const dbConfig = require("./config/config.json").development;
 
