939ac677aa95d77f0176d60a8f98a75696d3dd28
Praveen Sharma <praveen.sharma@decimal.co.in>
2025-03-22 00:06:11 +0530

    made emp_id unique

--- models/employee.js
+++ models/employee.js
@@ -4,7 +4,7 @@ const sequelize = require("../db");
 const Employee = sequelize.define("employee", {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     emp_name: { type: DataTypes.STRING, allowNull: false },
-    emp_id: { type: DataTypes.STRING, allowNull: false },
+    emp_id: { type: DataTypes.STRING, allowNull: false, unique: true },
 }, { schema: 'admin', timestamps: false });
 
 module.exports = Employee;
